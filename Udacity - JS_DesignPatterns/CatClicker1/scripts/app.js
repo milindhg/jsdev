044e8f3cdd928dde686ad1e7b7419727eed181b0
Milind Gokhale <milindhg@gmail.com>
2016-08-27 19:09:38 -0700

    added localStorage persistence

--- Udacity - JS_DesignPatterns/CatClicker1/scripts/app.js
+++ Udacity - JS_DesignPatterns/CatClicker1/scripts/app.js
@@ -1,4 +1,4 @@
-var cats = [ {
+var storageCats = [ {
 	name: 'kitty',
 	cnt: 0
 }, {
@@ -22,17 +22,50 @@ var imagePlaceholder;
 var catItemTemplStart = '<div class="text-center">';
 var catItemTemplEnd = '</div>';
 
+function initStorage() {
+	if ( !localStorage.cats ) {
+		localStorage.cats = JSON.stringify( cats );
+	}
+}
+
+function getStorageCats() {
+	if ( localStorage.cats ) {
+		return JSON.parse( localStorage.cats );
+	} else {
+		updateStorage();
+	}
+}
+
+function updateStorageCats( cat, count ) {
+	var index = findCatIndex( cat );
+	if ( index >= 0 ) storageCats[ index ].cnt = count;
+	updateStorage();
+}
+
+function updateStorage() {
+	localStorage.cats = JSON.stringify( storageCats );
+}
+
+function findCatIndex( cat ) {
+	for ( var index in storageCats ) {
+		if ( storageCats[ index ].name === cat )
+			return index;
+	}
+}
 
 document.addEventListener( 'DOMContentLoaded', function () {
+	initStorage();
+	storageCats = getStorageCats();
+	//console.log( storageCats );
 	var catList = document.getElementById( 'catList' );
-	for ( var index in cats ) {
+	for ( var index in storageCats ) {
 		var row = catList.insertRow();
-		row.id = cats[ index ].name;
+		row.id = storageCats[ index ].name;
 		var cell = row.insertCell();
-		cell.innerHTML = catItemTemplStart + cats[ index ].name + catItemTemplEnd;
+		cell.innerHTML = catItemTemplStart + storageCats[ index ].name + catItemTemplEnd;
 		cell = row.insertCell();
-		cell.innerHTML = cats[ index ].cnt;
-		cell.id = cats[ index ].name + '-cnt';
+		cell.innerHTML = storageCats[ index ].cnt;
+		cell.id = storageCats[ index ].name + '-cnt';
 		row.onclick = addImageEventListener;
 	}
 }, false );
@@ -48,7 +81,7 @@ function displayCat( cat ) {
 	}
 	var imgelem = document.createElement( "img" );
 	imgelem.id = cat.id + '-image';
-	imgelem.className = 'img-responsive img-rounded';
+	imgelem.className = 'img-responsive img-rounded center-block';
 	imgelem.src = 'images\\' + cat.id + '.jpg';
 	imgelem.onclick = increment.bind( cat.id );
 	imagePlaceholder.innerHTML = '';
@@ -56,11 +89,12 @@ function displayCat( cat ) {
 }
 
 var increment = function ( catname ) {
-	console.log(this);
+	//console.log( this );
 	var elem = document.getElementById( this + '-cnt' );
 	// console.log( 'clicked ' +
 	// 	this + ' ' + elem.innerHTML + ' times' );
-	var count=parseInt(elem.innerText);
-	count+=1;
-	elem.innerText=count;
+	var count = parseInt( elem.innerText );
+	count += 1;
+	updateStorageCats( elem.id.split('-')[0], count );
+	elem.innerText = count;
 };
