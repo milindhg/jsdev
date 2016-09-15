aafa41dc9f65134c20709f43d099563400819ce9
Milind Gokhale <milindhg@gmail.com>
2016-09-15 10:44:31 -0700

    completed LeetCode 27 and formatted headers of files

---
+++ LeetCode/LC27_Remove_Element.js
@@ -0,0 +1,50 @@
+/*
+ * Given an array and a value, remove all instances of that value in place and return the new
+ * length. Do not allocate extra space for another array, you must do this in place with constant
+ * memory. The order of elements can be changed. It doesn't matter what you leave beyond the new
+ * length.
+ *
+ * Example: Given input array nums = [3,2,2,3], val = 3
+ * Your function should return length = 2, with the first two elements of nums being 2.
+ *
+ */
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function ( nums, val ) {
+	var i = 0;
+	var j = nums.length - 1;
+	while ( nums[ j ] == val ) {
+		j -= 1;
+	}
+	while ( i <= j && j >= 0 ) {
+		if ( nums[ i ] == val ) {
+			var temp = nums[ i ];
+			nums[ i ] = nums[ j ];
+			nums[ j ] = temp;
+			i += 1;
+			j -= 1;
+			while ( nums[ j ] == val && j >= 0 ) {
+				j -= 1;
+			}
+			// console.log( i );
+			// console.log( j );
+			// console.log( nums );
+		} else {
+			i += 1;
+		}
+	}
+	// console.log(i);
+	// console.log(j);
+	// console.log(nums);
+	while ( nums[ j ] == val && j >= 0 ) {
+		j -= 1;
+	}
+	return j + 1;
+};
+
+// console.log( removeElement( [4,2,0,2,2,1,4,4,1,4,3,2], 4 ) );
+console.log( removeElement( [], 0 ) );
--- LeetCode/LC3_Longest_Substring.js
+++ LeetCode/LC3_Longest_Substring.js
@@ -10,34 +10,35 @@ Given "bbbbb", the answer is "b", with the length of 1.
 
 Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 */
+
 /**
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function ( s ) {
-	var queue = new Array( 0 );
-	var hm = {};
-	var maxlen = 0;
-	for ( var index in s ) {
-		if ( hm[ s[ index ] ] ) {
-			//console.log('present in hm');
-			var item = queue[ 0 ];
-			queue.shift();
-			delete hm[ item ];
-			while ( item != s[ index ] && queue.length > 0 ) {
-				item = queue[ 0 ];
-				queue.shift();
-				delete hm[ item ];
-			}
-		}
-		queue.push( s[ index ] );
-		hm[ s[ index ] ] = index;
-        console.log( queue.length, hm, queue );
-		if ( queue.length > maxlen ) {
-			maxlen = queue.length;
-		}
+var lengthOfLongestSubstring = function(s) {
+    var queue = new Array(0);
+    var hm = {};
+    var maxlen = 0;
+    for ( var index in s) {
+	if (hm[s[index]]) {
+	    // console.log('present in hm');
+	    var item = queue[0];
+	    queue.shift();
+	    delete hm[item];
+	    while (item != s[index] && queue.length > 0) {
+		item = queue[0];
+		queue.shift();
+		delete hm[item];
+	    }
 	}
-	return maxlen;
+	queue.push(s[index]);
+	hm[s[index]] = index;
+	console.log(queue.length, hm, queue);
+	if (queue.length > maxlen) {
+	    maxlen = queue.length;
+	}
+    }
+    return maxlen;
 };
 
-console.log( lengthOfLongestSubstring( 'tmmzuxt' ) );
+console.log(lengthOfLongestSubstring('tmmzuxt'));
