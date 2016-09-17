851db165129f847dd8eb3408fd524166f331ca1a
Milind Gokhale <milindhg@gmail.com>
2016-09-16 18:32:52 -0700

    Leetcode 6 completed

---
+++ LeetCode/LC6_Zigzag_Conversion.js
@@ -0,0 +1,64 @@
+/*
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string text, int nRows);
+convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+
+Solution: First, depending on number of rows given, create empty lists of numRows count. Then just keep a flag to increment or decrement rowNum index and keep pushing in that list. Done.
+
+*/
+
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+    if (numRows < 2) {
+	return s;
+    }
+    var arraymatrix = [];
+    for (var i = 0; i < numRows; i++) {
+	arraymatrix.push([]);
+    }
+    var rowToPushTo = 0;
+    var rowFlowUp = true;
+       
+    for ( var c in s) {
+	console.log(rowToPushTo);
+	arraymatrix[rowToPushTo].push(s[c]);
+	if (rowFlowUp) {
+	    rowToPushTo += 1;
+	} else {
+	    rowToPushTo -= 1;
+	}
+	if (rowToPushTo == numRows-1) {
+	    rowFlowUp = false;
+	} else if(rowToPushTo==0){
+	    rowFlowUp = true;
+	}
+	
+    }
+    var outputstring = "";
+    for ( var i in arraymatrix) {
+	for ( var j in arraymatrix[i]) {
+	    outputstring += arraymatrix[i][j];
+	}
+    }
+    return outputstring;
+
+};
+
+var main = function() {
+    var inputstring = 'PAYPALISHIRING';
+    var numRows = 4;
+    console.log(convert(inputstring, numRows));
+}
+
+main();
