09ebc32d5dacd29ee971fe946d5263f98499939e
Milind Gokhale <milindhg@gmail.com>
2016-10-16 23:30:43 -0700

    LeetCode 387 First Unique character in string completed

---
+++ LeetCode/LC387_First_Unique_Char_in_String.js
@@ -0,0 +1,58 @@
+/*
+
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+Note: You may assume the string contain only lowercase letters.
+
+
+Solution: First run a loop and note all character indexes. If repeat found, then update the count.
+Then loop on the hashmap and check first char entry with count 1.
+
+*/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function (s) {
+    hm = {};
+    var i = 0;
+    var retans = -1;
+    // Iterate through the string
+    while (i < s.length) {
+        // if character found again, increment its count
+        if (hm[s[i]]) {
+            hm[s[i]].cnt += 1;
+        }
+        // if character found first time , set it in map
+        else {
+            hm[s[i]] = {
+                index : i,
+                cnt : 1
+            };
+        }
+        i += 1;
+    }
+
+    for ( var char in hm) {
+        if (parseInt(hm[char].cnt) === 1) {
+            retans = hm[char].index;
+            return retans;
+        }
+    }
+    return retans;
+};
+
+var main = function () {
+    var s = "loveleetcode";
+    console.log(firstUniqChar(s));
+};
+
+main();
