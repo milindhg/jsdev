90bd70bcba926151accc1b13bea2443d64c75b38
Milind Gokhale <milindhg@gmail.com>
2016-10-18 10:23:08 -0700

    added a new approach suggested by Thube for problem 191. Still need to check the exact solution.

--- LeetCode/LC191_Number_Of_1_Bits.js
+++ LeetCode/LC191_Number_Of_1_Bits.js
@@ -10,14 +10,33 @@ For example, the 32-bit integer ’11' has binary representation 00000000000000000
  * @param {number} n - a positive integer
  * @return {number}
  * 
+ * this is a new way suggested by Thube. But did give some errors. Check this.
+ * 
+ */
+var hammingWeightCheck = function (n) {
+    var ans = 0;
+    if(n===Math.pow(2, 32)-1){
+        return 32;
+    }
+    while (n > 0) {
+        ans += 1;
+        n = (n & (n - 1));
+    }
+    return ans;
+};
+
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ * 
  * beats 49.23% javascript submissions
  * 
  */
-var hammingWeight = function(n) {
+var hammingWeight = function (n) {
     var ans = 0;
-    while(n>0){
-        ans=ans+n%2;
-        n=Math.floor(n/2);
+    while (n > 0) {
+        ans = ans + n % 2;
+        n = Math.floor(n / 2);
     }
     return ans;
 };
