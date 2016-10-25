b3037fa15e153f13d37a30471085c11d9eec41ff
Milind Gokhale <milindhg@gmail.com>
2016-10-24 23:48:44 -0700

    Leetcode 217 Contains Duplicate completed with unique kind of hashmap for constant time access. So that the algorithm is single pass.

---
+++ LeetCode/LC217_Contains_Duplicate.js
@@ -0,0 +1,33 @@
+/*
+
+Given an array of integers, find if the array contains any duplicates. Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function (nums) {
+    var hm = {
+        'unique' : {},
+        'duplicate' : {}
+    };
+    for ( var i in nums) {
+        if (hm['duplicate'][nums[i]]) {
+            hm['duplicate'][nums[i]] = hm['duplicate'][nums[i]] + 1;
+        } else if (hm['unique'][nums[i]]) {
+            hm['duplicate'][nums[i]] = hm['unique'][nums[i]] + 1;
+            delete hm['unique'][nums[i]];
+        } else {
+            hm['unique'][nums[i]] = 1;
+        }
+    }
+
+    if (Object.keys(hm['duplicate']).length === 0) {
+        return false;
+    }
+    return true;
+};
+
+
