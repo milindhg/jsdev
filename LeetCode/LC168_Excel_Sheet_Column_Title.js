d1d99222d34a62667649b9e8b2854778e02f5acf
milind.gokhale <milind.gokhale@servicenow.com>
2017-12-13 20:49:36 -0800

    completed leetcode 168 Number to Excel Sheet Column title as output.

---
+++ LeetCode/LC168_Excel_Sheet_Column_Title.js
@@ -0,0 +1,29 @@
+// https://leetcode.com/submissions/detail/131927260/
+//
+// Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+//
+// For example:
+//
+//     1 -> A
+//     2 -> B
+//     3 -> C
+//     ...
+//     26 -> Z
+//     27 -> AA
+//     28 -> AB
+
+/**
+ * @param {number} n
+ * @return {string}
+ */
+var convertToTitle = function(n) {
+  var map = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var answer = "";
+  while (n > 0) {
+    n--;
+    var character = map[(n % 26)];
+    answer = character + answer;
+    n = Math.floor(n / 26);
+  }
+  return answer;
+};
