2f3bbb67bae6cb329173ba158850aef1ba7bdf48
milind.gokhale <milind.gokhale@servicenow.com>
2018-05-18 09:33:34 -0700

    Completed leetcode 23 - merge K sorted lists.

---
+++ LeetCode/LC23_Merge_K_Sorted_Lists.js
@@ -0,0 +1,71 @@
+/* https://leetcode.com/problems/merge-k-sorted-lists/description/
+Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
+
+Example:
+
+Input:
+[
+  1->4->5,
+  1->3->4,
+  2->6
+]
+Output: 1->1->2->3->4->4->5->6
+
+ */
+
+/* 
+Solution:   https://leetcode.com/submissions/detail/154737744/  beats 100% js submissions.
+            Best way is to keep merging 2 lists at a time until 1 list is left. 
+            Use the given lists as a queue and keep removing 2 lists, merge them and enqueue the merged list back to queue.
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+var mergeKLists = function (lists) {
+    if (lists == null || lists.length == 0)
+        return [];
+    while (lists.length > 1) {
+        var list1 = lists.shift();
+        var list2 = lists.shift();
+        lists.push(mergeTwoLists(list1, list2));
+    }
+    return lists.shift();
+};
+
+var mergeTwoLists = function (l1, l2) {
+    if (l1 == null) return l2;
+    if (l2 == null) return l1;
+    var outputListHead = new ListNode(-1);
+    var tempNode = outputListHead;
+    while (l1 != null && l2 != null) {
+        if (l1.val <= l2.val) {
+            tempNode.next = l1;
+            tempNode = tempNode.next;
+            l1 = l1.next;
+        } else {
+            tempNode.next = l2;
+            tempNode = tempNode.next;
+            l2 = l2.next;
+        }
+    }
+    if (l1 != null) tempNode.next = l1;
+    if (l2 != null) tempNode.next = l2;
+    return outputListHead.next;
+};
+
+
+var print = function (list) {
+    while (list != null) {
+        console.log(list.val);
+        list = list.next;
+    }
+};
