d70241812e8373f88b78e9ee43408f1f34ab0a8b
Milind Gokhale <milindhg@gmail.com>
2018-10-30 16:17:41 -0700

    Completed leetcode 54 SPiral matrix and improved solution for leetcode59 Sprial matrix II

--- LeetCode/LC59_Spiral_Matrix_II.js
+++ LeetCode/LC59_Spiral_Matrix_II.js
@@ -18,6 +18,10 @@ Solution:   https://leetcode.com/submissions/detail/184957261/  beats 26% JS SUb
             Keep 2 indices, one for iterating over rows and one for iterating over columns
             Also keep 4 indices for maintaining min and max number of row and column.
             Now get the printed pattern by moving the 2 iterator indices within the limits of rows and columns and meanwhile keep modifying row and column min and max indices so that the pattern is formed is given.
+
+            https://leetcode.com/submissions/detail/186426502/  beats 100% JS Submissions.
+            The tweak of generating the array as we go instead of first creating an array with $ signs and then modifying that array improved the run time a lot. 
+            So looks like in javascript the run time array creation is better than creating an array and modifying the array.
  */
 
  /**
@@ -74,8 +78,63 @@ var generateMatrix = function(n) {
     return ansarr;
 };
 
+ /**
+ * @param {number} n
+ * @return {number[][]}
+ */
+var generateMatrixBetter = function(n) {
+    var rowStart = 0;
+    var rowEnd = n-1;
+    var colStart = 0;
+    var colEnd = n-1;
+    var num=1;
+    var ansarr = [];
+
+    while(rowStart<=rowEnd && colStart<=colEnd){
+        var i=rowStart;
+        var j=colStart
+        for(j=colStart; j<=colEnd; j++){
+            if(!ansarr[i]) ansarr[i] = [];
+            ansarr[i][j]=num++;
+        }
+        j--;
+        
+        rowStart++;
+
+        for(i=rowStart; i<=rowEnd; i++){
+            if(!ansarr[i]) ansarr[i] = [];
+            ansarr[i][j] = num++;
+        }
+        i--;
+        
+        colEnd--;
+        
+        for(j=colEnd; j>=colStart; j--){
+            if(!ansarr[i]) ansarr[i] = [];
+            ansarr[i][j] = num++;
+        }
+        j++
+        
+        rowEnd--;
+        
+        for(i=rowEnd; i>=rowStart; i--){
+            if(!ansarr[i]) ansarr[i] = [];
+            ansarr[i][j] = num++;
+        }
+        i++
+        
+        colStart++;
+
+    }
+    
+    return ansarr;
+};
+
+
 var main = function(){
     var arr = generateMatrix(3);
+    printArr(arr);
+    var arr = generateMatrixBetter(3);
     printArr(arr);
 };
 
