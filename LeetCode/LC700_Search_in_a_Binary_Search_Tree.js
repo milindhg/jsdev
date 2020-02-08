08371d97c7ece7a5821477bd9b46d67da128b0ad
milind.gokhale <milindhg@gmail.com>
2020-02-08 14:05:06 -0800

    completed lc700 very simple primitive recursive binary tree search

---
+++ LeetCode/LC700_Search_in_a_Binary_Search_Tree.js
@@ -0,0 +1,59 @@
+/* https://leetcode.com/problems/search-in-a-binary-search-tree/description/
+
+Given the root node of a binary search tree (BST) and a value. You need to find the node in the BST that the node's value equals the given value. Return the subtree rooted with that node. If such node doesn't exist, you should return NULL.
+
+For example, 
+
+Given the tree:
+        4
+       / \
+      2   7
+     / \
+    1   3
+
+And the value to search: 2
+You should return this subtree:
+
+      2     
+     / \   
+    1   3
+In the example above, if we want to search the value 5, since there is no node with value 5, we should return NULL.
+
+Note that an empty tree is represented by NULL, therefore you would see the expected output (serialized tree format) as [], not null.
+
+Solution:   https://leetcode.com/submissions/detail/301436278/  beats 85.10% JS Submissions.
+
+
+ */
+
+var TreeNode = require("../Utilities/TreeNode");
+var Tree = require("../Utilities/Tree");
+
+ /**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function(root, val) {
+    if(root==null) return null;
+    if(root.val==val) return root;
+    if(val<root.val) return searchBST(root.left,val);
+    if(val>=root.val) return searchBST(root.right,val);
+};
+
+var main = () => {
+    var t1 = Tree.prototype.buildBinaryTree([4,2,7,1,3]);
+    Tree.prototype.printBinaryTree(t1);
+    console.log(searchBST(t1,2));
+    console.log(searchBST(t1,22));
+
+};
+
+main();
