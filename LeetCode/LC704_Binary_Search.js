0a4572436b3089e4076b3ec698d4c410e01ca5e1
Milind Gokhale <milindhg@gmail.com>
2022-09-12 21:34:37 -0700

    Updated problem description details for problem 704 - Binary search in array.

--- LeetCode/LC704_Binary_Search.js
+++ LeetCode/LC704_Binary_Search.js
@@ -3,7 +3,43 @@
 https://leetcode.com/problems/binary-search/
 
 704. Binary Search
+Easy
 
+6412
+
+137
+
+Add to List
+
+Share
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [-1,0,3,5,9,12], target = 9
+Output: 4
+Explanation: 9 exists in nums and its index is 4
+Example 2:
+
+Input: nums = [-1,0,3,5,9,12], target = 2
+Output: -1
+Explanation: 2 does not exist in nums so return -1
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 < nums[i], target < 104
+All the integers in nums are unique.
+nums is sorted in ascending order.
+Accepted
+1,217,210
+Submissions
+2,207,259
 
 Solution:   https://leetcode.com/submissions/detail/735296872/ beats 90.71% js submissions.
             Simple binary search approach. Always keep looking for mid index and compare the target value with mid value. 
