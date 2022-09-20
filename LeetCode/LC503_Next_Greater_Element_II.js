de1427725d604b90f70f40b7c0621ee5bceacf2a
Milind Gokhale <milindhg@gmail.com>
2022-09-19 22:56:08 -0700

    Good problem to find the next greater element in the array on the right.

--- LeetCode/LC503_Next_Greater_Element_II.js
+++ LeetCode/LC503_Next_Greater_Element_II.js
@@ -13,12 +13,13 @@ Note: The length of given array won't exceed 10000.
 
 
 Solution:   https://leetcode.com/submissions/detail/215953004/  beats 96.27% JS Submissions.
+            https://leetcode.com/submissions/detail/800628148/  beats 100% JS Submissions.
 
             Get the max number in the array in linear time. Also note the index at which the max element was found. Try to get this index as the last/max index.
             Then come backwards from the noted index of max value. i.e. come from end of list to start of list in a circular fashion, i.e. if you reach 0th index, jump to last.
-            Keep storing the max element and the elements found on the way in stack.
-            If the current element is less than the top of stack, then note answer for that index as the top of stack.
-            If the current element is equal to or greater than the element on top of stack, then pop the element from stack.
+            Keep storing the max element and the elements found on the way in stack with below logic.
+                If the current element is less than the top of stack, then note answer for that index as the top of stack.
+                If the current element is equal to or greater than the element on top of stack, then pop the element from stack.
 
             Finally going by this approach, you're left with the answer.
             !!!Tricky part is to handle the scenarios where max element is repeated or some elements in the middle yet not max are repeated.
@@ -29,60 +30,60 @@ Solution:   https://leetcode.com/submissions/detail/215953004/  beats 96.27% JS 
  * @param {number[]} nums
  * @return {number[]}
  */
-var nextGreaterElements = function(nums) {
+var nextGreaterElements = function (nums) {
     var ans = new Array(nums.length).fill(-1);
-    if(nums.length==0) return [];
+    if (nums.length == 0) return [];
     var stk = [];
     var max = -Number.MAX_VALUE;
-    
+
     var maxIndex = -1;
     var i = 0;
-    while(i<nums.length){
-        if(max<=nums[i]){
-            max = nums[i];
-            maxIndex = i;
+    //get max number and index at which max number is present in the array.
+    nums.forEach((element, i) => {
+        element >= max ? maxIndex = i : maxIndex = maxIndex;
+        max = Math.max(max, element);
+    });
+
+    //Now go backwards from the index where we have max element in array.
+    var iterator = maxIndex;
+    stk.push(nums[iterator]);
+    iterator == 0 ? iterator = nums.length - 1 : iterator -= 1;
+    while (iterator != maxIndex) {
+        while (nums[iterator] >= stkPeek(stk)) {
+            stk.pop();
         }
-        i++;
-    }
-    
-    //Now go backwards
-    var maxIndexBkp = maxIndex;
-    stk.push(nums[maxIndex]);
-    if(maxIndex==0)
-        maxIndex = nums.length -1;
-    else
-        maxIndex = maxIndex - 1;
-    while(maxIndex!=maxIndexBkp){
-        var stkPeek = stk[stk.length-1];
-        while(stkPeek<=nums[maxIndex]){
-            stkPeek = stk[stk.length-1]
-            if(stkPeek<=nums[maxIndex])
-                stkPeek = stk.pop();
-        }
-        if(stk.length<1){
-            ans[maxIndex] = -1;
-            
-        }else{
-            ans[maxIndex] = stkPeek;
-        }
-        stk.push(nums[maxIndex]);
-        if(maxIndex==0)
-            maxIndex = nums.length -1;
-        else
-            maxIndex = maxIndex - 1;
+        stk.length == 0 ? ans[iterator] = -1 : ans[iterator] = stkPeek(stk);
+        stk.push(nums[iterator]);
+        iterator == 0 ? iterator = nums.length - 1 : iterator -= 1;
     }
     // console.log(max + ' ' + maxIndex + ' ' + ans);
     return ans;
 };
 
-var main = function(){
-    //nextGreaterElements([5,4,3,2,1]);
-    // nextGreaterElements([1,2,3,4,5]);
-    // nextGreaterElements([4,4,3,4,4]);
-    // nextGreaterElements([5,4,4,3,4]);
-    // console.log(nextGreaterElements([]));
-    // console.log(nextGreaterElements([1]));
-    console.log(nextGreaterElements([0]));
+let stkPeek = (stk) => {
+    return (stk.length > 0) ? stk[stk.length - 1] : "$";
+}
+
+var main = function () {
+    console.log(nextGreaterElements([1, 2, 1]));
+    console.log(nextGreaterElements([1, 2, 3, 4, 3]));
+    console.log(nextGreaterElements([1, 2, 6, 4, 3]));
+    console.log(nextGreaterElements([6, 2, 7, 4, 3]));
+    console.log(nextGreaterElements([6, 5, 4, 3, 2, 1]));
+    console.log(nextGreaterElements([1]));
+    console.log(nextGreaterElements([6, 6, 6, 6]));
+    console.log(nextGreaterElements([2, -3, -2, -1]));
+    console.log(nextGreaterElements([1, 3, 2, 2, 3, 3, 2]));
 };
 
 main();
+
+// [2,-1,2]
+// [2,3,4,-1,4]
+// [2,6,-1,6,6]
+// [7,7,-1,6,6]
+// [-1,6,6,6,6,6]
+// [-1]
+// [-1,-1,-1,-1]
+// [-1,-2,-1,2]
+// [3,-1,3,3,-1,-1,3]
